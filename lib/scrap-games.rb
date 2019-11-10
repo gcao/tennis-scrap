dc7c728d041fcfea8b2d07907708009505cbf8a8
Guoliang Cao <gcao99@gmail.com>
2019-11-10 11:07:06 -0500

    Scrap games

--- lib/scrap-games.rb
+++ lib/scrap-games.rb
@@ -4,22 +4,144 @@ require 'mechanize'
 require 'json'
 require 'ostruct'
 require 'cloudant_adapter'
+require 'pp'
 
-def translate_tournament_type type
-  if type =~ /DC/
-    'daviscup'
-  elsif type =~ /GS/
+# TODO:
+# Tournament:
+#   Name
+#   Location
+#   Start / end date
+#   Tournament type: grand slam, ATP Tour Finals, ATP 1000, 500, 250, Olympics, Davis Cup etc
+#   Court type: hard, clay, grass, carpet
+#   Indoor / outdoor
+# Game:
+#   Round
+#   Opponent name
+#   Opponent ranking
+#   Game result
+#   Walk over: true/false
+#   First round bye: true/false
+#   Date
+
+def translate_tournament_type type, name
+  if type =~ /_grandslam_/
     'grandslam'
-  elsif type =~ /WC/
+  elsif type =~ /_atp_finals/
     'atpfinal'
-  elsif type =~ /1000|SU/
+  elsif type =~ /_1000/
     'atp1000'
-  elsif type =~ /500/
+  elsif type =~ /_500/
     'atp500'
-  elsif type =~ /olympics/ or type =~ /ol\s*$/i
+  elsif type =~ /_250/
+    'atp250'
+  elsif name =~ /olympics/i
     'olympics'
   else
     'other'
+  end
+end
+
+# Set struct
+#   is_winner: true/false
+#   won_games
+#   lost_games
+#   is_tiebreak
+#   tiebreak_score
+def translate_score_to_set set_score
+  set = {}
+  if set_score =~ /-/
+    won_games, lost_games = set_score.split '-'
+  else
+    won_games = set_score[0]
+    lost_games = set_score[1]
+    if set_score =~ /^(76|67)(\d+)$/
+      set[:is_tiebreak] = true
+      set[:tiebreak_score] = $2
+    end
+  end
+
+  set[:is_winner] = won_games > lost_games
+  set[:won_games] = won_games
+  set[:lost_games] = lost_games
+
+  set
+end
+
+# TODO: update game info based on score
+#   format: best-of-three / best-of-five / other
+#   not_finished: true/false - whether myself or the opponent retire during the game
+#   is_walk_over: true/false
+#   is_deciding_set: true/false
+#   won_tie_breaks
+#   lost_tie_breaks
+#   won_bagels
+#   lost_bagels
+#   is_comeback_win: true/false
+#   is_comeback_lost: true/false
+#   sets:
+#     is_winner: true/false
+#     won_games
+#     lost_games
+#     is_tiebreak
+#     tiebreak_score
+#
+# Example game result
+# 63 676 75
+# 75 63
+# 46 46
+# 63 64
+# 46 764 60
+# 63 75
+#
+# 61 64 64
+# 63 64 64
+# 671 46 63 36
+# (W/O)
+#
+def update_game game
+  if game[:score] =~ /w\/o/i
+    game[:is_walk_over] = true
+    return
+  end
+
+  if game[:score] =~ /ret/i
+    game[:not_finished] = true
+  end
+
+  scores = game[:score].gsub(/[^\d -]/, '').split ' '
+  sets = scores.map{|score| translate_score_to_set score}
+  # game[:sets] = sets
+  game[:score] = game[:score].gsub(/76(\d+)/, '76(\1)').gsub(/67(\d+)/, '67(\1)')
+
+  sets_won = sets.count{|set| set[:is_winner] }
+  sets_lost = sets.count - sets_won
+  max = [sets_won, sets_lost].max
+  if max == 2
+    game[:format] = 'best-of-three'
+  elsif max == 3
+    game[:format] = 'best-of-five'
+  else
+    game[:format] = 'other'
+  end
+
+  # Tiebreaks
+  won_tiebreaks = sets.count{|set| set[:is_winner] && set[:is_tiebreak]}
+  if won_tiebreaks > 0
+    game[:won_tiebreaks] = won_tiebreaks
+  end
+  lost_tiebreaks = sets.count{|set| !set[:is_winner] && set[:is_tiebreak]}
+  if lost_tiebreaks > 0
+    game[:lost_tiebreaks] = lost_tiebreaks
+  end
+
+  # Bagels
+  won_bagels = sets.count{|set| set[:is_winner] && set[:is_bagel]}
+  if won_bagels > 0
+    game[:won_bagels] = won_bagels
+  end
+  lost_bagels = sets.count{|set| !set[:is_winner] && set[:is_bagel]}
+  if lost_bagels > 0
+    game[:lost_bagels] = lost_bagels
   end
 end
 
@@ -33,7 +155,7 @@ def scrap_year player, year
 
   is_first = true
   i = 100
-  page.search('.commonProfileContainer').each do |row|
+  page.search('.activity-tournament-table').each do |row|
     i -= 1
     break if i < 0
 
@@ -42,24 +164,59 @@ def scrap_year player, year
       next
     end
 
-    tournament_name = row.search('p:first-child a').text.strip
+    tournament_name = row.search('.title-content .tourney-title').text.strip
 
-    tournament_parts = row.search('p:first-child').text.strip.split(';')
-    next unless tournament_parts and tournament_parts.length > 0
+    tournament_dates = row.search('.tourney-dates').text.strip.split('-')
+    date = Date.parse tournament_dates[0]
+    location = row.search('.tourney-location').text.strip
 
-    date = Date.parse tournament_parts[1]
-    type = translate_tournament_type tournament_parts[2]
+    is_indoor = false
 
-    games = row.search('tr:not(.bioTableHead)').map do |tr|
+    court_type_parts = row.search('.tourney-details:nth-child(2) .item-details').text.gsub(/\r|\n/, '').strip.split(/ +/)
+    if court_type_parts.first == 'I'
+      is_indoor = true
+    end
+    court_type = court_type_parts.last
+
+    type_img = row.search('.tourney-badge-wrapper img').attr('src').value.strip
+    type = translate_tournament_type type_img, tournament_name
+
+    games = row.search('.mega-table tr').map do |tr|
       round = tr.search('td:first-child').text.strip
-      opponent = tr.search('td:nth-child(2) a').text.strip
-      rank = tr.search('td:nth-child(3)').text.strip
+      opponent = tr.search('td:nth-child(3) a').text.strip
+      rank = tr.search('td:nth-child(2)').text.strip
       rank = rank.to_i
       result = tr.search('td:nth-child(4)').text
       result = result =~ /^W/ ? 'W' : result =~ /^L/ ? 'L' : ''
-      {round: round, opponent: opponent, rank: rank, result: result}
+      score = tr.search('td:nth-child(5)').text.strip
+      game = {
+        round: round,
+        opponent: opponent,
+        rank: rank,
+        is_winner: result == 'W',
+        result: result,
+        score: score,
+      }
+      update_game game
+      game
     end
-    tournaments << {name: tournament_name, date: date, type: type, games: games}
+
+    if type == 'grandslam'
+      format = 'best-of-five'
+    else
+      format = 'best-of-three'
+    end
+
+    tournaments << {
+      name: tournament_name,
+      date: date,
+      location: location,
+      type: type,
+      format: format,
+      court_type: court_type,
+      is_indoor: is_indoor,
+      games: games
+    }
   end
 
   tournaments
@@ -69,20 +226,20 @@ def is_current_year? year
   year.to_i == Time.now.year
 end
 
-def scrap_player player
+def scrap_player player, start_year
   id = player.id + "_games"
   file_name = "../tennis/source/data/#{id}.json"
   result = {}
   if File.exists? file_name
     result = JSON.load File.open(file_name)
   end
-  result.delete 'data'
+  # result.delete 'data'
   result['generated_at'] = Time.now.to_i
   result['name'] = player.name
   result['tournaments'] ||= {}
 
   current_year = Time.now.year
-  current_year.downto(1995) do |year|
+  current_year.downto(start_year) do |year|
     year = year.to_s
     next if result['tournaments'][year] and not is_current_year? year
     result['tournaments'][year] = scrap_year player, year
@@ -102,31 +259,36 @@ players = [
   OpenStruct.new(
     id:   'roger_federer',
     name: 'Roger Federer',
-    url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Roger-Federer.aspx?t=pa&y=YEAR&m=s&e=0'
+    url:  'https://www.atptour.com/en/players/roger-federer/f324/player-activity?year=YEAR'
   ),
   OpenStruct.new(
     id:   'novak_djokovic',
     name: 'Novak Djokovic',
-    url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Novak-Djokovic.aspx?t=pa&y=YEAR&m=s&e=0'
+    url:  'https://www.atptour.com/en/players/novak-djokovic/d643/player-activity'
   ),
-  OpenStruct.new(
-    id:   'rafael_nadal',
-    name: 'Rafael Nadal',
-    url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Rafael-Nadal.aspx?t=pa&y=YEAR&m=s&e=0'
-  ),
-  OpenStruct.new(
-    id:   'andy_murray',
-    name: 'Andy Murray',
-    url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Andy-Murray.aspx?t=pa&y=YEAR&m=s&e=0'
-  ),
-  OpenStruct.new(
-    id:   'david_ferrer',
-    name: 'David Ferrer',
-    url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/David-Ferrer.aspx?t=pa&y=YEAR&m=s&e=0'
-  ),
+  # OpenStruct.new(
+  #   id:   'rafael_nadal',
+  #   name: 'Rafael Nadal',
+  #   url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Rafael-Nadal.aspx?t=pa&y=YEAR&m=s&e=0'
+  # ),
+  # OpenStruct.new(
+  #   id:   'andy_murray',
+  #   name: 'Andy Murray',
+  #   url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/Andy-Murray.aspx?t=pa&y=YEAR&m=s&e=0'
+  # ),
+  # OpenStruct.new(
+  #   id:   'david_ferrer',
+  #   name: 'David Ferrer',
+  #   url:  'http://www.atpworldtour.com/Tennis/Players/Top-Players/David-Ferrer.aspx?t=pa&y=YEAR&m=s&e=0'
+  # ),
 ]
 
-players.each do |player|
-  scrap_player player
+start_year = 1998
+
+if ARGV.length > 0
+  start_year = ARGV[0].to_i
 end
 
+players.each do |player|
+  scrap_player player, start_year
+end
--- lib/scrap-ytd.rb
+++ lib/scrap-ytd.rb
@@ -6,21 +6,24 @@ require 'json'
 require 'cloudant_adapter'
 
 agent = Mechanize.new
-page = agent.get('http://www.atpworldtour.com/Rankings/YTD-Singles.aspx')
+page = agent.get('https://www.atptour.com/en/rankings/singles-race-to-london')
 
 rankings = []
 is_first = true
-page.search('.rankingsContent tr').each do |row|
+page.search('.table-rankings tr').each do |row|
   if is_first
     is_first = false
     next
   end
 
-  rank = row.search('td:first-child span').text.to_i
-  last, first = row.search('td:first-child a').text.split(',')
+  rank = row.search('td:first-child').text.to_i
+  name = row.search('td:nth-child(4) a').text
+  i = name.rindex ' '
+  first = name[0..i-1]
+  last = name[i+1..-1]
   #name = "#{first.strip} #{last.strip}"
-  points = row.search('td:nth-child(2)').text.sub(',', '').to_i
-  rankings << {rank: rank, first: first[1..-1], last: last.strip, points: points}
+  points = row.search('td:nth-child(6)').text.sub(',', '').to_i
+  rankings << {rank: rank, first: first, last: last.strip, points: points}
 end
 
 id     = 'rankings_ytd'
